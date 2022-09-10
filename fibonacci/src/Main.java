e87cf605b5a0b74b8a7186b79c0f305d48deda08
Baffi Bálint <bafbal@gmail.com>
2022-09-10 04:18:01 +0200

    add fibonacci 7kyu 09/10

--- fibonacci/src/Main.java
+++ fibonacci/src/Main.java
@@ -1,16 +1,23 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(fib(1));
-    System.out.println(fib(2));
-    System.out.println(fib(3));
-    System.out.println(fib(4));
-    System.out.println(fib(5));
-    System.out.println(fib(6));
-    System.out.println(fib(7));
+    System.out.println(fib(1000));
   }
 
-  public static long fib (int n){
+  public static long fib2 (int n){
     return n < 3 ? 1 : fib(n - 1) + fib(n - 2);
+  }
+
+  public static long fib (int n) {
+    if (n < 3) {
+      return 1;
+    }
+    long f1 = 1;
+    long f2 = 1;
+    for (long i = 0; i < n - 3 ; i++) {
+      f2 = f1 + f2;
+      f1 = f2 - f1;
+    }
+    return f1 + f2;
   }
 }
